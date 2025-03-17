872e4b123947fdcf7e4a803502c0d353e65a3eee
KhoirulArifPratama <khoirularif932@gmail.com>
2025-03-17 11:06:39 +0700

    perulangan for pencacah turun

--- contoh_looping.cpp
+++ contoh_looping.cpp
@@ -14,8 +14,17 @@ int main()
     cout << "perulangan for pencacah naik" << endl;
     for (n = 0; n < 5; n++)
     {
-        cout << "Nilai n = " << n << "Selamat Datang" << endl;
+        cout << "Nilai n = " << n << " Selamat Datang " << endl;
     }
     cout << "Nilai terakhir = " << n << endl;
+    cout << endl;
+
+    cout << "perulangan for pencacah turun" << endl;
+    // perulangan dengan for
+    for (n = 5; n > 0; n--)
+    {
+        cout << "Nilai n = " << n << " Selamat Datang " << endl;
+    }
+    cout << " Nilai terakhir = " << n;
     cout << endl;
 }
